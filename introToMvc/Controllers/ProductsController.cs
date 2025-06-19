bd72d4c4ccd44f365d31e18b0441f6ea5d6f2e41
Osaid-Raddad <osaidislam1@gmail.com>
2025-06-19 15:51:30 +0300

    Validation on create Form

--- introToMvc/Controllers/ProductsController.cs
+++ introToMvc/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using introToMvc.Data;
+using introToMvc.Models;
 namespace introToMvc.Controllers
 {
     public class ProductsController : Controller
@@ -20,8 +21,40 @@ namespace introToMvc.Controllers
 
         public ViewResult Create()
         {
-            return View("Create");
+            return View("Create",new Product());
         }
 
+        public IActionResult Add(Product request)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(request);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Create",request);
+            }
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product is null)
+            {
+                return View("NotFound");
+            }
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ViewResult NotFound()
+        {
+            return View("NotFound");
+        }
     }
 }
--- introToMvc/Models/Product.cs
+++ introToMvc/Models/Product.cs
@@ -1,11 +1,20 @@
-﻿namespace introToMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace introToMvc.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
         public string? Name { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
         public string? Description { get; set; }
-
+        [Required]
+        [Range(0.01, 100000.00)]
         public decimal Price { get; set; }
 
         public int? CategoryId {get; set; }
